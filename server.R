8ebb3889f0fdf1c88cdbced9ebab0b0e66e896ad
Derek <derek.stefanik@icloud.com>
2021-01-15 16:22:13 -0500

    split App.R into server.R

--- server.R
+++ server.R
@@ -17,40 +17,6 @@ library(shinyFiles)
 #Please note that the download button doesn't works well on the RStudio viewer, so launch the app in a browser if you want to have the original file name as default.
 
 
-
-ui <- navbarPage("Downloads",
-    
-    # set navbar                 
-    tabPanel("E. lineata",
-    
-    # set the title
-    titlePanel("NGS Data Download Panel"),
-    
-    # set the side panel
-    sidebarPanel(
-    shinyFilesButton('files', label='File select', title='Please select a file', multiple=T),
-    verbatimTextOutput('rawInputValue'),
-    verbatimTextOutput('filepaths') ,
-    downloadButton("downloadFiles", "Download Files")),
-    
-    # main panel
-    mainPanel(
-        h1("Sequence Data Information"),
-        
-        p("The Illunina sequencing reads and assembled transcriptome described in the 2014 BMC Genomics paper can be downloaded using the interface in the sidebar to the left."),
-        br(),
-        p("All sequencing data pertaining to",
-            em("E. lineata"),
-          "generated during my PhD training in The Finnerty Lab, including unpublished sequencing reads from life stages and body regions, are included within the folders accessible through this site."),
-        br(),
-        h2("About this app"),
-        p("This web app is built with the Shiny framework within R.  The file selection portion utilizes the shinyFiles library, while the download button uses the basic downloadhandler from the shiny library"),
-        p("The app is hosted on an AWS Elastic Compute Server.")
-        
-        )
-    
-)
-)
 server <- function(input, output) {
 
   roots =  c(wd = '~/Desktop/Anemones/filedownloader/')
@@ -73,5 +39,3 @@ server <- function(input, output) {
     }
   )
 }
-
-shinyApp(ui = ui , server = server)
